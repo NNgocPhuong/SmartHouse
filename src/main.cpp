1e8af9230bb3c3328ef800de872c27f589b6f587
NguyenNgocPhuong <phuong260722@gmail.com>
2024-09-17 17:00:21 +0700

    bản thầy sửa rồi

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 #include <HTTPClient.h>
 #include "../include/System/System.h"
 
+int status = 0;
+void set_status(int v);
+
+void turn_on() {
+  set_status(1);
+}
+void turn_off() {
+  set_status(0);
+}
+
 const int JSON_LEN = 1024;
 char jbuf[JSON_LEN] = { };
 
@@ -10,7 +20,49 @@ char Hex(int a) {
   if (a < 10) return char(a | '0');
   return char(a + 55);
 }
+
+class Response {
+  int sPost, mPost;
+  LPCHAR source;
+
+  int find(int start = -1) {
+    for (int i = start + 1; char c = source[i]; i++) {
+      if (c == ':') return i;
+    }
+    return -1;
+  }
+
+public:
+  Response(const String& s) : source(s.c_str()) { 
+    sPost = mPost = -1;
+    int i = find();
+    if (i < 0) return;
+
+    sPost = find(i);
+    if (sPost < 0) return;
+
+    mPost = find(sPost);
+  }
+
+  int Value(int i) const {
+
+    if (i < 0) return 0;
+
+    int a = 0;
+    for (int k = i + 1; char c = source[k]; k++) {
+      if (c == ' ' && a == 0) continue;
+      if (c > '9' || '0' > c) break;
+      a = (a << 1) + (a << 3) + (c & 15);
+    }
+    return a;
+  }
+
+  int Status() const { return Value(sPost); }
+  int Mins() const { return Value(mPost); }
+};
+
 int FindValue(String s, int pos) {
+
     int val = 0;
     for (int i = 0; char c = s[i]; i++) {
         if (c == ':') {
@@ -50,13 +102,25 @@ public:
   JBUF& Value(LPCHAR s) { *this << '\"' << s << '\"'; return *this; }
   JBUF& Number(int v) { *this << v; return *this; }
 
+  LPCHAR CreateApiBody(LPCHAR action) {
+    Start()
+      .Name("url").Value(action).Comma()
+      .Name("value")
+        .Open()
+          .Name("_id").MAC().Comma()
+          .Name("s").Number(status)
+        .Close()
+    .End();
+
+    return ToString();
+  }
+
 } json;
 
 // put function declarations here:
 LPCHAR ssid = "Mit Nam";
 LPCHAR password = "09082805";
 LPCHAR serverName = "https://slock.vst.edu.vn/api/all";
-int status = 0;
 
 HTTPClient http;
 Log _log;
@@ -65,57 +129,66 @@ System _system;
 class Running : public Counter {
   void on_interrupt() override {
     //tắt máy bơm và gửi trạng thái cho server
+    turn_off();
   }
-} running;
 
-class RequestTimer : public Timer
-{
 public:
-  RequestTimer() : Timer(3000){}
-  
-  void on_restart() override{
-    json.Start()
-      .Name("url").Value("connect/check").Comma()
-      .Name("value")
-        .Open()
-          .Name("_id").MAC()
-        .Close()
-    .End();
-      
-    
-    int httpCode = http.POST(LPCHAR(json));
-    int s = FindValue(http.getString(), 2);
-    _log << s << endl;
-    if((s & 1) != 0)
-    {
-      status = 1;
-      // kích 1 chân relay lên
-      running.Start(FindValue(http.getString(), 3) * 60000);
-      json.Start()
-      .Name("url").Value("connect/changed").Comma()
-      .Name("value")
-        .Open()
-          .Name("_id").MAC().Comma()
-          .Name("s").Number(status)
-        .Close()
-      .End();
-    }
-    else
-    {
-      status = 0;
-    }
+  static int post(LPCHAR action) {
+    _log << "--- post " << action << " ---" << endl;
+    return http.POST(json.CreateApiBody(action));
+  }
+} may_bom;
 
-    //running.Start(3000);
-    
+class RequestTimer : public SecondCounter
+{
+public:  
+  void on_one_second() override{   
+    const int timeout = 3;
+    _log << "Send checking in " << (timeout - Value()) << " seconds" << endl;
+
+    if (Value() == timeout) {
+      Reset();
+ 
+      int httpCode = Running::post("connect/check");
+      if (httpCode != 200) {
+        _log << "Server error" << endl;
+        turn_off();
+        return;
+      }
+
+      String body = http.getString();
+      Response res(body);
+
+      _log << body << endl;
+
+      int s = res.Status() & 1;
+      if (s != status) {
+        set_status(s);
+        if (s) {
+          int m = res.Mins();
+          may_bom.Start(m * 60000);
+        }
+      }
+
+      return;
+    }
   }
 } rqTimer;
 
+void set_status(int v) {
+  if (status != v) {
+    status = v;
+    _log << "status = " << status << endl;
+    Running::post("connect/changed");
+  }
+}
 
 void setup() {
   
   Serial.begin(9600);
   WiFi.begin(ssid,password);
   _system.Reset();
+
   while (WiFi.status() == WL_CONNECTED)
   {
     delay(500);
