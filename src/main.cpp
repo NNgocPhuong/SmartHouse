3ec632ae9343080cf2f81cb1f047ae94cf20cc88
NguyenNgocPhuong <phuong260722@gmail.com>
2024-09-17 15:30:08 +0700

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,21 @@ char Hex(int a) {
   if (a < 10) return char(a | '0');
   return char(a + 55);
 }
+int FindValue(String s, int pos) {
+    int val = 0;
+    for (int i = 0; char c = s[i]; i++) {
+        if (c == ':') {
+            --pos;
+            c = s[++i];
+            continue;
+        }
+        if (pos == 0) {
+            if (c < '0' || c > '9') break;
+            val = val * 10 + int(c & 15);
+        }
+    }
+    return val;
+}
 class JBUF : public Buffer {
 public:
   JBUF() : Buffer(jbuf, JSON_LEN) {}
@@ -33,12 +48,15 @@ public:
   JBUF& Comma() { *this << ','; return *this; }
   JBUF& Name(LPCHAR s) { *this << '\"' << s << '\"' << ':'; return *this; }
   JBUF& Value(LPCHAR s) { *this << '\"' << s << '\"'; return *this; }
+  JBUF& Number(int v) { *this << v; return *this; }
+
 } json;
 
 // put function declarations here:
 LPCHAR ssid = "Mit Nam";
 LPCHAR password = "09082805";
 LPCHAR serverName = "https://slock.vst.edu.vn/api/all";
+int status = 0;
 
 HTTPClient http;
 Log _log;
@@ -46,14 +64,14 @@ System _system;
 
 class Running : public Counter {
   void on_interrupt() override {
-    _log << "Stop" << endl;
+    //tắt máy bơm và gửi trạng thái cho server
   }
 } running;
 
 class RequestTimer : public Timer
 {
 public:
-  RequestTimer() : Timer(5000){}
+  RequestTimer() : Timer(3000){}
   
   void on_restart() override{
     json.Start()
@@ -64,10 +82,31 @@ public:
         .Close()
     .End();
       
-    _log << LPCHAR(json) << endl;
+    
     int httpCode = http.POST(LPCHAR(json));
-    _log << httpCode << endl;
-    running.Start(3000);
+    int s = FindValue(http.getString(), 2);
+    _log << s << endl;
+    if((s & 1) != 0)
+    {
+      status = 1;
+      // kích 1 chân relay lên
+      running.Start(FindValue(http.getString(), 3) * 60000);
+      json.Start()
+      .Name("url").Value("connect/changed").Comma()
+      .Name("value")
+        .Open()
+          .Name("_id").MAC().Comma()
+          .Name("s").Number(status)
+        .Close()
+      .End();
+    }
+    else
+    {
+      status = 0;
+    }
+
+    //running.Start(3000);
+    
   }
 } rqTimer;
 
